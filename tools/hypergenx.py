4ab3d2a6bdde0782871975edf9ef604524125034
NotShroudX97 <67467393+NotShroudX97@users.noreply.github.com>
2021-12-02 19:01:12 +0530

    Update hypergenx.py

--- tools/hypergenx.py
+++ tools/hypergenx.py
@@ -19,6 +19,6 @@ API_HASH = input("Enter API_HASH: ")
 with Client(':memory:', api_id=API_KEY, api_hash=API_HASH) as app:
 	app.send_message(
 	    "me",
-	    f"#HyperGenX | Repl - https://replit.com/@NotShroudX97/HyperGenX | @UsergeXForkUpdates | #HU_STRING_SESSION\n\n```{app.export_session_string()}```"
+	    f"#HyperGenX | Repl - https://replit.com/@NotShroudX97/HyperGenX | @UsergeXForkUpdates | Noice!, #HyperGenX String Session Has Been Generated & Sent To Your Saved Messages | Note:- Don't Share This With Anyone Else ID Hacked! | #HU_STRING_SESSION\n\n```{app.export_session_string()}```"
 	)
 	print("Noice!, #HyperGenX String Session Has Been Generated & Sent To Your Saved Messages | Note:- Don't Share This With Anyone Else ID Hacked!")
