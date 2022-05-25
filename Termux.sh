822df4ca8d11ae7d1229c09caa17bcf1c1194179
Ryuk <90273333+anonymous-x97@users.noreply.github.com>
2022-05-25 13:19:53 +0530

    Update Termux.sh

--- Termux.sh
+++ Termux.sh
@@ -1,13 +1,13 @@
 yes|pkg upgrade && apt-get update &&
 pkg install -y git python3 && 
-pip3 install python-dotenv 
-&& pip3 install wheel 
-&& pip3 install pyrogram==1.4.7 
-&& pkg update -y python3 
-&& pip3 install tgcrypto 
-&& pip3 install poetry-core==1.0.7 
-&& pip3 install wget 
-&& pip install --upgrade pip &&
+pip3 install python-dotenv &&
+pip3 install wheel &&
+pip3 install pyrogram==1.4.7 &&
+pkg update -y python3 &&
+pip3 install tgcrypto &&
+pip3 install poetry-core==1.0.7 &&
+pip3 install wget &&
+pip install --upgrade pip &&
 rm -rf string && 
-git clone https://github.com/anonymous-x97/HyperGenX string 
-&& clear && cd string && bash gen
+git clone https://github.com/anonymous-x97/HyperGenX string &&
+clear && cd string && bash gen
